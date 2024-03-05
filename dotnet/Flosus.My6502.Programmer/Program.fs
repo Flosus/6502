3a9dd2198e20fce40e8887c44e085e9ca92881d0
Flosus <flosus.mail@gmail.com>
2024-03-05 01:12:02 +0100

    more impl
    @ 14 bytes/second

--- dotnet/Flosus.My6502.Programmer/Program.fs
+++ dotnet/Flosus.My6502.Programmer/Program.fs
@@ -9,12 +9,30 @@ open Flosus.My6502.Lib
 open Flosus.My6502.Lib.SerialManagement
 
 module EEPROMProgrammer =
+    
+    let mutable start = DateTime.Now
+    
 
     type Context =
         { SerialPort: SerialPort
           mutable IsReady: bool
+          mutable IsLastBinary: bool
+          mutable Binary: byte array
           Messages: ConcurrentQueue<byte> }
 
+    let writeFile (ctx: Context) =
+        let time = DateTime.Now - start
+        let seconds = time.TotalSeconds
+        let maxValue = UInt16.MaxValue
+        let binLength = ctx.Binary.Length
+        let binDouble = double ctx.Binary.Length
+        let speed = binDouble/(double seconds)
+        let missing = maxValue - (uint16 binLength)
+        let expectedTime = (double missing) / speed
+        let percent = Math.Round ((double 100) * binDouble / (double maxValue), 3)
+        printfn $"Writing bin; sec={seconds} length={ctx.Binary.Length} speed={speed}bytes/s"
+        printfn $"{binLength}/{maxValue} = {percent}%%; expectedSeconds={Math.Round (expectedTime, 2)}s"
+        File.WriteAllBytes ("G:\\dev\\6502\EEPROM.BIN", ctx.Binary)
 
     let waitForReady (port: Context) =
         while not port.IsReady do
@@ -22,7 +40,9 @@ module EEPROMProgrammer =
     
     let sendAndWait (port: Context) (cmd:string) =
         waitForReady port
+        Thread.Sleep 100
         port.SerialPort.Write($"{cmd}\n")
+        Thread.Sleep 100
         port.IsReady <- false
         waitForReady port
 
@@ -54,11 +74,23 @@ module EEPROMProgrammer =
 
                 match bt with
                 | b when b > 0 && b <= int Byte.MaxValue ->
-                    ctx.Messages.Enqueue(byte bt)
-                    printf $"{char bt}"
-
+                    ctx.Messages.Enqueue(byte bt)                    
                     if ctx.Messages.Count % 64 = 0 then
-                        printfn ""
+                        let array = ctx.Messages.ToArray ()
+                        ctx.Messages.Clear ()
+                        if ctx.IsLastBinary then
+                            let ctxBinary = ctx.Binary
+                            ctx.Binary <- Array.append ctxBinary array
+                            writeFile ctx
+                            ctx.IsLastBinary <- false
+                        else 
+                            let charArray =  array |> Array.map (fun b -> char b)
+                            let str = String(charArray)
+                            let binStartMsg = "BIN_START0000000000000000000000000000000000000000000000000000000"
+                            ctx.IsLastBinary <- str = binStartMsg
+                            if not (str.StartsWith "rdy") then
+                                printfn $"{str}"
+                                
                         ctx.IsReady <- true
                 | _ -> printfn "INVALID READ BYTE"
             with
@@ -77,15 +109,17 @@ module EEPROMProgrammer =
         let ctx: Context =
             { SerialPort = port
               IsReady = false
+              IsLastBinary = false
+              Binary = [||] 
               Messages = ConcurrentQueue<byte>() }
-
+        start <- DateTime.Now
         let run () = read ctx
         let thread = Thread(run)
         port.Open()
         thread.Start()
 
         port.NewLine <- "\n'"
-        readAll ctx |> ignore
+        readAll ctx
 
         ctx.IsReady <- false
         waitForReady ctx
